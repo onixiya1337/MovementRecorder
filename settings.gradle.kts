93ac3951a8e8d5caac0304c79f530d61da3ac86a
onixiya1337 <47077455+onixiya1337@users.noreply.github.com>
2023-08-20 11:42:21 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        maven("https://repo.polyfrost.cc/releases")
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.fabricmc.net")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.minecraftforge.net")
+    }
+}
+
+rootProject.name = "ExampleMod"
